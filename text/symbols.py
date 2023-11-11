7f4f0293e7cf0f2e3a0458af51a1c2e64d2f43eb
Nur Hikmah <44131310+nrhikmah@users.noreply.github.com>
2023-11-11 20:56:44 +0800

    Update symbols.py

--- text/symbols.py
+++ text/symbols.py
@@ -9,10 +9,11 @@ from text import cmudict
 _pad        = '_'
 _punctuation = '!\'()᨞᨟:;? '
 _special = '-'
-_letters = 'ᨀᨁᨂᨃᨄᨅᨆᨇᨈᨉᨊᨋᨌᨍᨎᨏᨐᨒᨑᨓᨔᨕᨖ'
+_letters = ['ᨀ',
+'ᨁ','ᨂ','ᨃ','ᨄ','ᨅ','ᨆ','ᨇ','ᨈ','ᨉ','ᨊ','ᨋ','ᨌ','ᨍ','ᨎ','ᨏ','ᨐ','ᨑ','ᨒ','ᨓ','ᨔ','ᨕ','ᨖ','ᨗ','ᨘ','ᨙ','ᨚ','ᨛ','᨞','᨟']
 
 # Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
 _arpabet = ['@' + s for s in cmudict.valid_symbols]
 
 # Export all symbols:
-symbols = [_pad] + list(_special) + list(_punctuation) + list(_letters) + _arpabet
+symbols = [_pad] + list(_special) + list(_punctuation) + _letters + _arpabet
